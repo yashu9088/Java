611abdcc82ca1903eef7e18363101cd1e484d8cd
Yashavanth Kapila <73021364+yashu9088@users.noreply.github.com>
2025-06-30 11:58:01 +0530

    Update PositiveOrNegative.java

--- PositiveOrNegative.java
+++ PositiveOrNegative.java
@@ -7,3 +7,19 @@ class PositiveOrNegative{
         System.out.println("Entered number is " + ((num>0)?"positive":(num==0?"zero":"negative")));
     }
 }
+
+
+// To Check Array 
+// public static void main (String[] args){
+//         Scanner sc = new Scanner(System.in);
+//         System.out.println("Enter a array size");
+//         int n = sc.nextInt();
+//         int a[] = new int[n];
+//         for(int i=0;i<n;i++){
+//             a[i] = sc.nextInt();
+//         }
+//         System.out.println("Entered numbers are ");
+//         for(int i=0;i<n;i++){
+//             System.out.println(a[i] + " is " +((a[i]>0)?"positive":(a[i]==0?"zero":"negative")));
+//         }
+//     }
