fee526451fddca2698c128ad5eb4bc91ad962933
Yashavanth Kapila <73021364+yashu9088@users.noreply.github.com>
2025-12-16 23:30:44 +0530

    Delete spring-workshop/src/main/java/org/geeksforgeeks/spring_workshop directory

--- spring-workshop/src/main/java/org/geeksforgeeks/spring_workshop/repository/UserRepository.java
+++
@@ -1,7 +0,0 @@
-package org.geeksforgeeks.spring_workshop.repository;
-
-import org.geeksforgeeks.spring_workshop.entity.User;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface UserRepository extends JpaRepository<User, Integer> {
-}
